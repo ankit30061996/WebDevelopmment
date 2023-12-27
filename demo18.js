8de702b3de8b43748c661e8dfc57e9e5cd8fbdc6
ankit30061996 <154228991+ankit30061996@users.noreply.github.com>
2023-12-27 15:56:21 +0530

    Add files via upload

---
+++ demo18.js
@@ -0,0 +1,13 @@
+/*const x = document.getElementById('heading');
+console.log(x);
+const y = document.getElementsByClassName('heading');
+console.log(y);
+console.log(y[0]);
+console.log(y[1]);
+const z = document.getElementsByTagName('h1');
+console.log(z);
+document.querySelector('#myName').style.color ="red";
+*/
+setTimeout(()=>{
+    document.querySelector('#myName').style.color ="red";
+},3000);
